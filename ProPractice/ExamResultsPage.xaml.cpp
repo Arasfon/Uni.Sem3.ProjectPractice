beac097acc9a64e845c62f9f5be9f28f4c3f981f
Egor Boyarkin <egrsfn@gmail.com>
2023-10-16 15:26:38 +0300

    Add cards to simplify UI for exam results

--- ProPractice/ExamResultsPage.xaml.cpp
+++ ProPractice/ExamResultsPage.xaml.cpp
@@ -8,6 +8,8 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::Media;
 
 namespace winrt::ProPractice::implementation
 {
@@ -21,7 +23,6 @@ namespace winrt::ProPractice::implementation
         _examController = unbox_value<ExamController>(e.Parameter());
 
         int correctCount = 0;
-        std::wostringstream resultsText;
 
         for (unsigned int i = 0; i < _examController.Questions().Size(); i++)
         {
@@ -38,14 +39,40 @@ namespace winrt::ProPractice::implementation
                 }
             }
 
-            resultsText << L"Ответ " << i + 1 << L" — " << (isCorrect ? L"верный" : L"неверный") << std::endl;
+            StackPanel sp;
+            sp.Padding({ 16, 16, 16, 16 });
+
+            TextBlock questionNumberTextBlock;
+            questionNumberTextBlock.Text(L"Вопрос " + std::to_wstring(i + 1));
+            questionNumberTextBlock.TextWrapping(TextWrapping::NoWrap);
+            questionNumberTextBlock.Width(120);
+            questionNumberTextBlock.Style(unbox_value<Microsoft::UI::Xaml::Style>(Application::Current().Resources().Lookup(box_value(L"SubtitleTextBlockStyle"))));
+
+            sp.Children().Append(questionNumberTextBlock);
+
+            TextBlock isCorrectTextBlock;
+
+            if (isCorrect)
+            {
+                sp.Background(unbox_value<Brush>(Application::Current().Resources().Lookup(box_value(L"SystemFillColorSuccessBackgroundBrush"))));
+                isCorrectTextBlock.Text(L"Верный ответ");
+            }
+            else
+            {
+                sp.Background(unbox_value<Brush>(Application::Current().Resources().Lookup(box_value(L"SystemFillColorCriticalBackgroundBrush"))));
+                isCorrectTextBlock.Text(L"Неверный ответ");
+            }
+
+            sp.Children().Append(isCorrectTextBlock);
+            ResultsGridView().Items().Append(sp);
+
             correctCount += isCorrect ? 1 : 0;
         }
 
-        ResultTextBlock().Text(resultsText.str() + L"\nРезультат: " + std::to_wstring(correctCount) + L"/" + std::to_wstring(_examController.Questions().Size()));
+        ResultTextBlock().Text(L"Результат: " + std::to_wstring(correctCount) + L"/" + std::to_wstring(_examController.Questions().Size()));
     }
 
-    void ExamResultsPage::ResetExamButtonClick(IInspectable const&, Microsoft::UI::Xaml::RoutedEventArgs const&)
+    void ExamResultsPage::ResetExamButtonClick(IInspectable const&, RoutedEventArgs const&)
     {
         _examController.CallControl(ExamControlAction::Reset);
     }
